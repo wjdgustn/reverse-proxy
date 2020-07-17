7f7792bf9224430811a5b8fae3a79da1f2c092f7
Hyunsoo, Chung <admin@hyonsu.com>
2020-07-17 13:31:15 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const http = require('http');
+const https = require('https');
+const httpProxy = require('http-proxy');
+const fs = require('fs');
+
+const setting = require('./setting.json');
+
+const app = express();
+const proxy = httpProxy.createProxyServer();
+
+let sub_http;
+if (setting.USE_SSL) sub_http = express();
+
+proxy.on('proxyReq', (proxyReq, req, res, options) => {
+    proxyReq.setHeader('X-Real-IP', req.connection.remoteAddress);
+    proxyReq.setHeader('HTTP_VIA', req.connection.remoteAddress);
+    proxyReq.setHeader('HTTP_X_FORWARD_FOR', req.connection.remoteAddress);
+    proxyReq.setHeader('x-forwarded-for', req.connection.remoteAddress);
+});
+
+
+app.use((req, res, next) => {
+    console.log(`${req.connection.remoteAddress}가 https://${req.hostname}:${setting.HTTPS_PORT}${req.url}에 접속하였습니다.`);
+    const domain = JSON.parse(fs.readFileSync('./domain.json'));
+
+    if(domain.hasOwnProperty(req.hostname)) {
+        proxy.web(req, res, {
+            target: domain[req.hostname]
+        });
+        return;
+    }
+    else {
+        res.send('<h1>잘못된 접근입니다!</h1>');
+    }
+});
+
+sub_http.use((req, res, next) => {
+    const domain = JSON.parse(fs.readFileSync('./domain.json'));
+
+    if(domain.hasOwnProperty(req.hostname)) {
+        res.redirect(`https://${req.hostname}:${setting.HTTPS_PORT}${req.url}`);
+        return;
+    }
+    else {
+        res.send('<h1>잘못된 접근입니다!</h1>');
+    }
+});
+
+if(setting.USE_SSL) {
+    let options = {
+        cert: fs.readFileSync(setting.SSL_CERT),
+        key: fs.readFileSync(setting.SSL_KEY)
+    }
+    https.createServer(options, app).listen(setting.HTTPS_PORT, () => {
+        console.log('보안 서버가 구동중입니다!');
+    });
+    http.createServer(sub_http).listen(setting.HTTP_PORT, () => {
+        console.log("보조 HTTP 서버가 구동중입니다!");
+    });
+}
+else {
+    http.createServer(app).listen(setting.HTTP_PORT, () => {
+        console.log("서버가 구동중입니다!");
+    });
+}
